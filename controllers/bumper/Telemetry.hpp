11bc248bed759e2e9f564f83bf2c89e475001285
Thomas Harrison <twh2898@vt.edu>
2023-08-19 19:47:53 -0400

    Add documentation to Telemetry.hpp

--- controllers/bumper/Telemetry.hpp
+++ controllers/bumper/Telemetry.hpp
@@ -10,21 +10,48 @@ namespace roomba {
 
     using std::string;
 
+    /**
+     * @brief Interface for classes to send data though Telemetry.
+     */
     class TelemetrySender {
     public:
+        /**
+         * @brief Get a json object with telemetry data.
+         *
+         * @return json telemetry object
+         */
         virtual json getTelemetry() const = 0;
     };
 
+    /**
+     * @brief UDP Telemetry Broadcaster.
+     */
     class Telemetry {
         UDPClient udp;
 
     public:
+        /**
+         * @brief Connect to Telemetry
+         *
+         * @param udpPort the UDP telemetry port
+         * @param udpAddress the UDP telemetry network address
+         */
         Telemetry(int udpPort, string udpAddress) : udp(udpPort, udpAddress) {}
 
+        /**
+         * @brief Send arbitrary telemetry data
+         *
+         * @param data json telemetry object
+         */
         void send(json data) {
             udp.send_message(data.dump());
         }
 
+        /**
+         * @brief Send telemetry from a TelemetrySender via getTelemetry().
+         *
+         * @param sender the TelemetrySender
+         */
         void send(TelemetrySender * sender) {
             send(sender->getTelemetry());
         }
