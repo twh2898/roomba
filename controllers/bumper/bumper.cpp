4f54fc0d010e6e1dfe0be4b747d1727654b52956
Thomas Harrison <twh2898@vt.edu>
2023-08-20 12:49:45 -0400

    Add spdlog to bumper.cpp

--- controllers/bumper/bumper.cpp
+++ controllers/bumper/bumper.cpp
@@ -13,6 +13,10 @@ using namespace webots;
 #include <vector>
 using namespace std;
 
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+
 #include "Localizer.hpp"
 #include "MotionControl.hpp"
 #include "PID.hpp"
@@ -25,10 +29,22 @@ using namespace roomba;
 #define TIME_STEP 64
 
 int main() {
+    vector<spdlog::sink_ptr> sinks;
+    sinks.push_back(make_shared<spdlog::sinks::stdout_color_sink_mt>());
+    sinks.push_back(make_shared<spdlog::sinks::basic_file_sink_mt>("log.txt"));
+    auto combined_logger =
+        make_shared<spdlog::logger>("bumper", begin(sinks), end(sinks));
+    spdlog::set_default_logger(combined_logger);
+    spdlog::set_level(spdlog::level::debug);
+    spdlog::info("Logging enabled");
+
     fstream f("config.json");
     json config = json::parse(f);
 
-    cout << "Config " << config.dump() << endl;
+    {
+        string c = config.dump();
+        spdlog::debug("Config: {}", c);
+    }
 
     int movementCounter = 0;
     int leftSpeed, rightSpeed;
