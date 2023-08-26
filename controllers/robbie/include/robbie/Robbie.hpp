5e92490a03ad843f6f27e07b9fefb39a66bf2c2d
Thomas Harrison <twh2898@vt.edu>
2023-08-26 01:58:16 -0400

    Platform and Localizer only Robbie

--- controllers/robbie/include/robbie/Robbie.hpp
+++ controllers/robbie/include/robbie/Robbie.hpp
@@ -1,5 +1,25 @@
 #pragma once
 
+#include "Localizer.hpp"
+// #include "MotionControl.hpp"
+// #include "Planning.hpp"
+#include "Platform.hpp"
+#include "Telemetry.hpp"
+// #include "World.hpp"
+
 namespace robbie {
-    class Robbie {};
+    class Robbie : public TelemetrySender {
+    public:
+        Platform platform;
+        Localizer local;
+        // WorldModel world;
+        // PathPlanning planner;
+        // MotionControl mc;
+
+        Robbie();
+
+        void step(int duration);
+
+        json getTelemetry() const override;
+    };
 }
--- controllers/robbie/src/Robbie.cpp
+++ controllers/robbie/src/Robbie.cpp
@@ -1,3 +1,20 @@
 #include "robbie/Robbie.hpp"
 
-namespace robbie {}
+namespace robbie {
+    Robbie::Robbie() : platform(), local() {}
+
+    void Robbie::step(int duration) {
+        platform.step(duration);
+        local.update(platform);
+    }
+
+    json Robbie::getTelemetry() const {
+        json res;
+        res.update(platform.getTelemetry());
+        res.update(local.getTelemetry());
+        // res.update(world.getTelemetry());
+        // res.update(planner.getTelemetry());
+        // res.update(mc.getTelemetry());
+        return res;
+    }
+}
