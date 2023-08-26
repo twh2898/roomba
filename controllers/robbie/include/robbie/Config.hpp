f65c2edf35b296762d67e6aeafe35960ddcf140b
Thomas Harrison <twh2898@vt.edu>
2023-08-26 01:09:52 -0400

    Add config structure

--- controllers/robbie/include/robbie/Config.hpp
+++ controllers/robbie/include/robbie/Config.hpp
@@ -1,5 +1,38 @@
 #pragma once
 
+#include <map>
+#include <string>
+
 #include "json.hpp"
 
-namespace robbie {}
+namespace robbie {
+    using json = nlohmann::json;
+
+    using std::string;
+    using std::map;
+
+    struct TelemetryConfig {
+        int port;
+        string address;
+    };
+
+    struct PIDConfig {
+        double p;
+        double i;
+        double d;
+        double speed;
+    };
+
+    struct Config {
+        json data;
+
+        TelemetryConfig telemetry;
+        string pidMode;
+        bool tuneMode;
+        map<string, PIDConfig> pid;
+
+        PIDConfig & getPID() const;
+
+        static Config fromFile(const string & file);
+    };
+}
--- controllers/robbie/src/Config.cpp
+++ controllers/robbie/src/Config.cpp
@@ -1,3 +1,16 @@
 #include "robbie/Config.hpp"
 
-namespace robbie {}
+#include <fstream>
+
+namespace robbie {
+    using std::fstream;
+
+    Config::Config() {}
+
+    Config Config::fromFile(const string & file) {
+        fstream f(file);
+        json config = json::parse(f);
+
+        // TODO: parse structure
+    }
+}
