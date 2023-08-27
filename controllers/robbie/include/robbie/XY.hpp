9e74673550fd6cc9a30f44a47b08f1357dddf767
Thomas Harrison <twh2898@vt.edu>
2023-08-27 02:50:42 -0400

    Add length method

--- controllers/robbie/include/robbie/XY.hpp
+++ controllers/robbie/include/robbie/XY.hpp
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <cmath>
+
 namespace robbie {
+    using std::sqrt;
+
     struct XY {
         double x;
         double y;
 
         XY(double x = 0, double y = 0) : x(x), y(y) {}
+
+        double length(const XY & other) {
+            double dx = other.x - x;
+            double dy = other.y - y;
+            return sqrt(dx * dx + dy * dy);
+        }
     };
 }
